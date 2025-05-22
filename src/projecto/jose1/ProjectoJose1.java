8400cd669e9933fa7a52f145f1d37d0f531cede2
Jose Caoç <joselitus05@exemple.com>
2025-05-22 17:29:11 +0200

    first commit

---
+++ src/projecto/jose1/ProjectoJose1.java
@@ -0,0 +1,11 @@
+package projecto.jose1;
+
+public class ProjectoJose1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola");
+        System.out.println("Adios");
+    }
+    
+}
